3b8d3f0595ede33623e0ea09ef1969cd30c969a6
Himanshu Singh <himanshu.singh.200717@gmail.com>
2026-08-22 12:32:24 +0530

    Time: 0 ms (100%), Space: 7.9 MB (75.28%) - LeetHub

---
+++ 2544-alternating-digit-sum/2544-alternating-digit-sum.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int alternateDigitSum(int n) {
+        int sum=0;
+        string s=to_string(n);
+        for(int i=0;i<s.size();i++){
+            if(i%2==0)
+            sum+=s[i]-'0';
+            else
+            sum-=s[i]-'0';
+
+        }
+        return sum;
+        
+    }
+};
