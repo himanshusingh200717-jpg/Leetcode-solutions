8aaf6cbc42df27adb68db8b1428ec68a31f5dc6e
Himanshu Singh <himanshu.singh.200717@gmail.com>
2026-08-25 07:54:09 +0530

    Time: 0 ms (100%), Space: 23.9 MB (94.59%) - LeetHub

---
+++ 3718-smallest-missing-multiple-of-k/3718-smallest-missing-multiple-of-k.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int missingMultiple(vector<int>& nums, int k) {
+        sort(nums.begin(),nums.end());
+        int count=1;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]==k*count){
+                count++;
+            }
+
+        }
+        return k*count;
+        
+    }
+};
